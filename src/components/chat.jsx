f0f84e99f013f0ca9f0887673f1637b54b9ad96b
radhechaudhary <radhechaudhary6398@gmai.com>
2025-02-04 22:57:00 +0530

    port

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -81,7 +81,7 @@ function Chat() {
         const handleOnline=() => {   // Reconnect to the socket
             if (!socket.connected && localStorage.getItem('username')) {
                 socket.connect();
-                socket.once("connect", () => {
+                socket.on("connect", () => {
                     console.log("Socket reconnected!");
                     socket.emit('get_saved_messages', localStorage.getItem('username'))
                 })
