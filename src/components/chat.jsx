fd5f9ac475f92214cfe0fccf756c9327e51ed612
radhechaudhary <radhechaudhary6398@gmai.com>
2025-02-14 19:47:25 +0530

    multiple

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -24,7 +24,7 @@ const socket = io("https://chat-app-backend-production-bd09.up.railway.app", {
     transports: ["websocket"], // ✅ Force WebSockets instead of polling
     withCredentials: true,
     reconnection: true,        // ✅ Enable automatic reconnection
-    reconnectionAttempts: 10,   // ✅ Try reconnecting up to 5 times
+    reconnectionAttempts: 100,   // ✅ Try reconnecting up to 5 times
     reconnectionDelay: 2000,   // ✅ Wait 2 seconds before retrying
     reconnectionDelayMax: 5000 // ✅ Max delay of 5 seconds
 });
